40e4044edd6cb5c74cd0e6d6871ef3b6f7251817
Atiyaali <atiyaali653@gmail.com>
2026-06-20 15:34:29 +0500

    .

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,7 +1,7 @@
 import com.example.Deploy
 def call(){
 def deployer = new Deploy(this)
-def server = "ec2-user@13.234.34.0"
+def server = "ec2-user@43.205.129.49"
 deployer.sshDeploy(
         server,
         env.VERSION
